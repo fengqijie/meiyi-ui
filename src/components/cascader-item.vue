ba05131dee6a671df14e58b50dbbdc42a71cd964
fengqijie@guazi.com <fengqijie@guazi.com>
2019-07-10 15:14:40 +0800

    粗糙显示级联下拉内容

---
+++ src/components/cascader-item.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="cascader-item">
+        {{ optionsItem.name }}
+        <div v-if="optionsItem.children">
+            <cascader-item
+                v-for="item in optionsItem.children"
+                :key="item.code"
+                :optionsItem="item"
+            ></cascader-item>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'cascader-item',
+    props: {
+        optionsItem: {
+            type: Object
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.cascader-item {
+    border: 1px solid red;
+    margin: 5px;
+}
+</style>
+
+
--- src/components/cascader.vue
+++ src/components/cascader.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="cascader">
-        <slot></slot>
+        <div class="trigger">
+            <slot></slot>
+        </div>
+        <div class="popover">
+            <div v-for="item in options" :key="item.code">
+                <cascader-item :optionsItem="item"></cascader-item>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import CascaderItem from './cascader-item'
 export default {
-    name: 'Mcascader'
+    name: 'Mcascader',
+    components: { CascaderItem },
+    props: {
+        options: {
+            type: Array
+        }
+    }
 }
 </script>
 
